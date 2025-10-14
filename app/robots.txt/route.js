5b77acd25a4fec9229f6e3d7bd416fcefdefc1e9
Dakota <skatetapeco@gmail.com>
2025-10-14 14:21:40 -0500

    SEO: add HEAD handlers for sitemap.xml and robots.txt

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -5,3 +5,7 @@ Sitemap: https://hotdish-planner.vercel.app/sitemap.xml
 `;
   return new Response(body, { headers: { 'Content-Type': 'text/plain' } });
 }
+
+export async function HEAD() {
+  return new Response(null, { headers: { 'Content-Type': 'text/plain' } });
+}
--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -6,3 +6,7 @@ ${urls.map(u => `<url><loc>${u}</loc></url>`).join('')}
 </urlset>`;
   return new Response(body, { headers: { 'Content-Type': 'application/xml' } });
 }
+
+export async function HEAD() {
+  return new Response(null, { headers: { 'Content-Type': 'application/xml' } });
+}
